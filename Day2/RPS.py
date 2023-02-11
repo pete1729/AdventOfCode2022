99e6e92778a7305b982aafcc8b719d86396cf63a
Peter Mathews <peter1729@gmail.com>
2023-02-11 09:33:12 -0800

    Day 2 code

---
+++ Day2/RPS.py
@@ -0,0 +1,26 @@
+with open('/home/peter/Code/AdventOfCode2022/Day2/input', 'r') as reader:
+# Read & print the first 5 characters of the line 5 times
+    lines = reader.readlines()
+    print(len(lines))
+    total_score = 0
+
+    for line in lines:
+        if line[0] == "A" and line[2] == "X":
+            total_score += 4
+        elif  line[0] == "A" and line[2] == "Y":
+            total_score += 8
+        elif  line[0] == "A" and line[2] == "Z":
+            total_score += 3
+        elif  line[0] == "B" and line[2] == "X":
+            total_score += 1
+        elif  line[0] == "B" and line[2] == "Y":
+            total_score += 5
+        elif  line[0] == "B" and line[2] == "Z":
+            total_score += 9
+        elif  line[0] == "C" and line[2] == "X":
+            total_score += 7
+        elif  line[0] == "C" and line[2] == "Y":
+            total_score += 2
+        elif  line[0] == "C" and line[2] == "Z":
+            total_score += 6
+    print(total_score)
