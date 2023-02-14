79027720979e65fdf80078a620b04dbd98b742e3
Peter Mathews <peter1729@gmail.com>
2023-02-13 16:53:58 -0800

    Part 1 complete

--- Day5/Stack.py
+++ Day5/Stack.py
@@ -23,33 +23,36 @@ with open('/home/peter/Code/AdventOfCode2022/Day5/input', 'r') as reader:
         stack_contents.append([])
     print(stack_contents)
 
-    line_3 = re.split(r'\W+', lines[3])
-    line_3.remove('')
-    line_3.remove('')
-    print(line_3)
-
     start_line = base_line_num -1
     print("Start line:", start_line)
     print(lines[start_line])
 
-
-    for current_line in range(start_line,0,-1):
+    for current_line in range(start_line,-1,-1):
         for i in range(len(stack_contents)):
             next_crate = lines[current_line][4*i+1]
             if next_crate != " ":
                 stack_contents[i].append(next_crate)
-        print("Stack contents", stack_contents)
+    
+    print("Stack contents", stack_contents)
 
-my_list = [3,4,5]
-my_list.append(6)
-print(my_list)
-print(my_list.pop())
-print(my_list)
+    for line_num in range(base_line_num+2,len(lines)):
+        print(lines[line_num])
+        if lines[line_num][6] == "":
+            num_to_move = int(lines[line_num][5])
+        else:
+            num_to_move = int(lines[line_num][5:7])
+        source_stack = int(lines[line_num][-7])
+        dest_stack = int(lines[line_num][-2])
+        print("Num to move:", num_to_move)
+        print("Source stack:", source_stack)
+        print("Destination stack:", dest_stack)
 
-list_of_lists = []
-list_of_lists.append([])
-list_of_lists.append([1,2,3])
-list_of_lists.append([4,5,6])
-list_of_lists[1].append(7)
-print(list_of_lists)
-print(list_of_lists[0])
+        for i in range(num_to_move):
+            stack_contents[dest_stack-1].append(stack_contents[source_stack-1].pop())
+        print(stack_contents)
+        print()
+
+    out_message = ""
+    for i in range(len(stack_contents)):
+        out_message = out_message + stack_contents[i][-1]
+    print(out_message)
