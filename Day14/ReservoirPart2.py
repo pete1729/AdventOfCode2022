723cdd51a07fc1c5e9c824e3a2d1471c386df3aa
Peter Mathews <peter1729@gmail.com>
2023-03-05 19:07:50 -0800

    Completed Day 14

--- Day14/ReservoirPart2.py
+++ Day14/ReservoirPart2.py
@@ -6,6 +6,7 @@ with open('/home/peter/Code/AdventOfCode2022/Day14/input', 'r') as reader:
 slice = np.zeros((1000,1000), dtype=int)
 
 def process_input(lines):
+    highest_y_coord = 0
     for line in lines:
         points_with_whitespace = line.split(" -> ")
         endpoints = [point_with_whitespace.strip() for point_with_whitespace in points_with_whitespace]
@@ -15,7 +16,11 @@ def process_input(lines):
             start_point_str = endpoints[i].split(",")
             end_point_str = endpoints[i+1].split(",")
             start_point = np.array([int(start_point_str[0]),int(start_point_str[1])])
+            if start_point[1] > highest_y_coord:
+                highest_y_coord = start_point[1]
             end_point = np.array([int(end_point_str[0]),int(end_point_str[1])])
+            if end_point[1] > highest_y_coord:
+                highest_y_coord = end_point[1]
 
             num_steps = int(np.linalg.norm(end_point-start_point)+0.1)
             step = (end_point - start_point) / num_steps
@@ -23,8 +28,20 @@ def process_input(lines):
 
             for j in range(num_steps + 1):
                 next_step = start_point + j*step
-                print(next_step)
                 slice[next_step[0]][next_step[1]] = 1
+
+    start_point = np.array([0,highest_y_coord+2])
+    end_point = np.array([999, highest_y_coord+2])
+
+    num_steps = int(np.linalg.norm(end_point-start_point)+0.1)
+    step = (end_point - start_point) / num_steps
+    step = np.array([int(step[0]), int(step[1])])
+
+    for j in range(num_steps + 1):
+        next_step = start_point + j*step
+        slice[next_step[0]][next_step[1]] = 1
+
+    print("Highest y coordinate", highest_y_coord)
     
 
 
@@ -39,9 +56,6 @@ def process_sand_movement():
         while(sand_still_moving == 1):
             if slice[current_sand_point[0], current_sand_point[1]+1] == 0:
                 current_sand_point = np.array([current_sand_point[0], current_sand_point[1]+1])
-                if current_sand_point[1] == slice.shape[1]-1:
-                    sand_still_moving = 0
-                    finished = 1
             elif slice[current_sand_point[0]-1, current_sand_point[1]+1] == 0:
                 current_sand_point = np.array([current_sand_point[0]-1, current_sand_point[1]+1])
             elif slice[current_sand_point[0]+1, current_sand_point[1]+1] == 0:
@@ -50,7 +64,9 @@ def process_sand_movement():
                 sand_still_moving = 0
                 sand_at_rest += 1
                 slice[current_sand_point[0], current_sand_point[1]] = 1
-                print("New filled in spot:", current_sand_point[0], current_sand_point[1])
+                if current_sand_point[0] == 500 and current_sand_point[1] == 0:
+                    finished = 1
+                #print("New filled in spot:", current_sand_point[0], current_sand_point[1])
     print("Number of sand particles at rest:", sand_at_rest)
 
 
